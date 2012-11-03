3546601707f6e0b4c0253230ca918cd896ece5c4
koszoaron <koszoaron@gmail.com>
2012-11-03 23:56:23 +0100

    Updated graphics, motor speed display

--- maguScada/src/com/github/koszoaron/maguscada/ScadaActivity.java
+++ maguScada/src/com/github/koszoaron/maguscada/ScadaActivity.java
@@ -18,11 +18,23 @@ public class ScadaActivity extends Activity implements OnClickListener {
     private Button btnA2;
     private Button btnA3;
     private Button btnA4;
+    private Button btnA5;
+    private Button btnB1;
+    private Button btnB2;
+    private Button btnB3;
+    private Button btnB4;
+    private Button btnB5;
     
     private int imageLevel = 0;
     private int semaphoreRed = 0;
     private int semaphoreYellow = 0;
     private int semaphoreGreen = 0;
+    
+    public enum ButtonSet {
+        FIRST_SET,
+        SECOND_SET
+    }
+    private ButtonSet currentButtonSet = ButtonSet.FIRST_SET;
 
     /* (non-Javadoc)
      * @see android.app.Activity#onCreate(android.os.Bundle)
@@ -44,17 +56,32 @@ public class ScadaActivity extends Activity implements OnClickListener {
         ivSemaphoreGreen.setImageLevel(semaphoreGreen);
         
         btnA1 = (Button)findViewById(R.id.btnA1);
+        btnA2 = (Button)findViewById(R.id.btnA2);
+        btnA3 = (Button)findViewById(R.id.btnA3);
+        btnA4 = (Button)findViewById(R.id.btnA4);
+        btnA5 = (Button)findViewById(R.id.btnA5);
+        btnB1 = (Button)findViewById(R.id.btnB1);
+        btnB2 = (Button)findViewById(R.id.btnB2);
+        btnB3 = (Button)findViewById(R.id.btnB3);
+        btnB4 = (Button)findViewById(R.id.btnB4);
+        btnB5 = (Button)findViewById(R.id.btnB5);
+        
         btnA1.setText("Up/Down");
         btnA1.setOnClickListener(this);
-        btnA2 = (Button)findViewById(R.id.btnA2);
+        
         btnA2.setText("Red");
         btnA2.setOnClickListener(this);
-        btnA3 = (Button)findViewById(R.id.btnA3);
+        
         btnA3.setText("Yellow");
         btnA3.setOnClickListener(this);
-        btnA4 = (Button)findViewById(R.id.btnA4);
+        
         btnA4.setText("Green");
         btnA4.setOnClickListener(this);
+        
+        btnB5.setText("...");
+        btnB5.setOnClickListener(this);
+        
+        switchButtonSets();
     }
 
     /* (non-Javadoc)
@@ -90,7 +117,20 @@ public class ScadaActivity extends Activity implements OnClickListener {
                 semaphoreGreen = 0;
             }
             ivSemaphoreGreen.setImageLevel(semaphoreGreen);
+        } else if (v == btnB5) {
+            switchButtonSets();
         }
     }
 
+    private void switchButtonSets() {
+        if (currentButtonSet == ButtonSet.FIRST_SET) {
+            currentButtonSet = ButtonSet.SECOND_SET;
+            btnB1.setText("other function");
+            btnB2.setText("other function");
+        } else if (currentButtonSet == ButtonSet.SECOND_SET) {
+            currentButtonSet = ButtonSet.FIRST_SET;
+            btnB1.setText("first function");
+            btnB2.setText("first function");
+        }
+    }
 }
