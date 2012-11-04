d46c548872ba60dd377b38b90c737c71a6e7ab3e
koszoaron <koszoaron@gmail.com>
2012-11-04 17:45:06 +0100

    Revised button handling

--- maguScada/src/com/github/koszoaron/maguscada/ScadaActivity.java
+++ maguScada/src/com/github/koszoaron/maguscada/ScadaActivity.java
@@ -29,12 +29,6 @@ public class ScadaActivity extends Activity implements OnClickListener {
     private int semaphoreRed = 0;
     private int semaphoreYellow = 0;
     private int semaphoreGreen = 0;
-    
-    public enum ButtonSet {
-        FIRST_SET,
-        SECOND_SET
-    }
-    private ButtonSet currentButtonSet = ButtonSet.FIRST_SET;
 
     /* (non-Javadoc)
      * @see android.app.Activity#onCreate(android.os.Bundle)
@@ -66,22 +60,23 @@ public class ScadaActivity extends Activity implements OnClickListener {
         btnB4 = (Button)findViewById(R.id.btnB4);
         btnB5 = (Button)findViewById(R.id.btnB5);
         
-        btnA1.setText("Up/Down");
         btnA1.setOnClickListener(this);
-        
-        btnA2.setText("Red");
         btnA2.setOnClickListener(this);
-        
-        btnA3.setText("Yellow");
         btnA3.setOnClickListener(this);
-        
-        btnA4.setText("Green");
         btnA4.setOnClickListener(this);
-        
-        btnB5.setText("...");
+        btnA5.setOnClickListener(this);
+        btnB1.setOnClickListener(this);
+        btnB2.setOnClickListener(this);
+        btnB3.setOnClickListener(this);
+        btnB4.setOnClickListener(this);
         btnB5.setOnClickListener(this);
         
-        switchButtonSets();
+        btnA1.setText("Up/Down");
+        btnA2.setText("Red");
+        btnA3.setText("Yellow");
+        btnA4.setText("Green");
+        btnB5.setText("...");
+
     }
 
     /* (non-Javadoc)
@@ -90,47 +85,59 @@ public class ScadaActivity extends Activity implements OnClickListener {
     @Override
     public void onClick(View v) {
         if (v == btnA1) {
+            
             if (imageLevel == 0) {
                 imageLevel = 1;
             } else if (imageLevel == 1){
                 imageLevel = 0;
             }
             ivMain.setImageLevel(imageLevel);
+        
         } else if (v == btnA2) {
+        
             if (semaphoreRed == 0) {
                 semaphoreRed = 1;
             } else if (semaphoreRed == 1){
                 semaphoreRed = 0;
             }
             ivSemaphoreRed.setImageLevel(semaphoreRed);
+        
         } else if (v == btnA3) {
+            
             if (semaphoreYellow == 0) {
                 semaphoreYellow = 1;
             } else if (semaphoreYellow == 1){
                 semaphoreYellow = 0;
             }
             ivSemaphoreYellow.setImageLevel(semaphoreYellow);
+        
         } else if (v == btnA4) {
+            
             if (semaphoreGreen == 0) {
                 semaphoreGreen = 1;
             } else if (semaphoreGreen == 1){
                 semaphoreGreen = 0;
             }
             ivSemaphoreGreen.setImageLevel(semaphoreGreen);
+        
+        } else if (v == btnA5) {
+            
+            
+        } else if (v == btnB1) {
+            
+            
+        } else if (v == btnB2) {
+            
+            
+        } else if (v == btnB3) {
+            
+            
+        } else if (v == btnB4) {
+            
+            
         } else if (v == btnB5) {
-            switchButtonSets();
-        }
-    }
-
-    private void switchButtonSets() {
-        if (currentButtonSet == ButtonSet.FIRST_SET) {
-            currentButtonSet = ButtonSet.SECOND_SET;
-            btnB1.setText("other function");
-            btnB2.setText("other function");
-        } else if (currentButtonSet == ButtonSet.SECOND_SET) {
-            currentButtonSet = ButtonSet.FIRST_SET;
-            btnB1.setText("first function");
-            btnB2.setText("first function");
+            
+            
         }
     }
 }
